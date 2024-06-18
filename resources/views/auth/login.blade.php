2728e7611fefd776a1174e3c115d2f9d0c140660
purnamadyh11 <purnamadyh@gmail.com>
2024-06-18 19:30:33 +0700

    Add Auth

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+<div class="col-md-4 col-md-offset-4">
+            <h2 class="text-center"><b>Rental</b><br>Aplikasi Rental Tempat</h3>
+            <hr>
+            @if(session('error'))
+            <div class="alert alert-danger">
+                <b>Opps!</b> {{session('error')}}
+            </div>
+            @endif
+            <form action="{{ route('actionlogin') }}" method="post">
+            @csrf
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" placeholder="Password" required="">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Log In</button>
+                <hr>
+                <p class="text-center">Belum punya akun? <a href="/register">Register</a> sekarang!</p>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
